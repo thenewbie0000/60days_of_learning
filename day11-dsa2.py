103033485b8f8f269405c5d97c9418c627ccc96c
Rojesh Shrestha <87111687+thenewbie0000@users.noreply.github.com>
2022-04-24 21:29:16 +0545

    Add files via upload

---
+++ day11-dsa2.py
@@ -0,0 +1,29 @@
+# BINARY SEARCH VERSION 1
+
+def binary_search(list, target):
+    first = 0
+    last = len(list) - 1
+
+    while first <= last:
+        midpoint = (first + last)//2    # // means floor division
+
+        if list[midpoint] == target:
+            return midpoint
+        elif list[midpoint] <  target:
+            first = midpoint + 1
+        elif list[midpoint] > target:
+            last = midpoint - 1
+        
+    return None
+
+
+def verify(index):
+    if index is not None:
+        print("Target found at index:", index)
+    else:
+        print("Target not Found in list")
+
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+result = binary_search(numbers, 10)
+verify(result)
